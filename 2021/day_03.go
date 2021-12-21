27992c22a21af2a119b2441fd83f3e671d1e9f0a
pies-n-loaf <amwright.dev@gmail.com>
2021-12-20 22:12:38 -0600

    Complete day 3 part 2

--- 2021/day_03.go
+++ 2021/day_03.go
@@ -10,12 +10,20 @@ import (
 type Day03 struct{}
 
 func (d Day03) Solve(scanner *bufio.Scanner) {
+	var zeroes, ones []string
 	var bitCounter []int
 	var total int
 
 	for scanner.Scan() {
-		bits := strings.Split(scanner.Text(), "")
+		bitString := scanner.Text()
+		bits := strings.Split(bitString, "")
 		total++
+
+		if bitString[0] == '1' {
+			ones = append(ones, bitString)
+		} else {
+			zeroes = append(zeroes, bitString)
+		}
 
 		for i, bit := range bits {
 			if bitCounter == nil || i >= len(bitCounter) {
@@ -27,10 +35,20 @@ func (d Day03) Solve(scanner *bufio.Scanner) {
 		}
 	}
 
+	var oxygen, co2 string
+
+	if len(zeroes) > len(ones) {
+		oxygen = findLifeSupportRating(zeroes, false)
+		co2 = findLifeSupportRating(ones, true)
+	} else {
+		oxygen = findLifeSupportRating(ones, false)
+		co2 = findLifeSupportRating(zeroes, true)
+	}
+
+	fmt.Printf("The life support rating is: %d\n", parseResults(oxygen, co2))
+
 	var gamma, epsilon string
 	half := total / 2
-
-	fmt.Printf("%d+, half: %d", bitCounter, half)
 
 	for _, count := range bitCounter {
 		if count > half {
@@ -42,12 +60,52 @@ func (d Day03) Solve(scanner *bufio.Scanner) {
 		}
 	}
 
-	fmt.Printf("total: %d, gamma: %s, epsilon: %s", total, gamma, epsilon)
+	fmt.Printf("The power consumption is: %d\n", parseResults(gamma, epsilon))
+}
 
-	gammaInt, _ := strconv.ParseInt(gamma, 2, 64)
-	epsilonInt, _ := strconv.ParseInt(epsilon, 2, 64)
+func findLifeSupportRating(bits []string, inverse bool) string {
+	rating := string(bits[0][0])
+	columns := len(bits[0])
+	// Starts at one because we filter out the first column during the initial scan
+	for i := 1; i < columns; i++ {
+		bigger, smaller := filterBits(bits, i)
+		if inverse && len(smaller) != 0 {
+			rating += string(smaller[0][i])
+			bits = smaller
+		} else {
+			rating += string(bigger[0][i])
+			bits = bigger
+		}
+		if len(bits) == 1 {
+			return bits[0]
+		}
+	}
+	return rating
+}
 
-	fmt.Printf("gamma: %d, epsilon: %d", gammaInt, epsilonInt)
+func filterBits(bits []string, column int) (biggerBits []string, smallerBits []string) {
+	var zeroBits, oneBits []string
+	for _, bit := range bits {
+		if bit[column] == '1' {
+			oneBits = append(oneBits, bit)
+		} else {
+			zeroBits = append(zeroBits, bit)
+		}
+	}
 
-	fmt.Printf("The power consumption is: %d\n", gammaInt*epsilonInt)
+	if len(zeroBits) > len(oneBits) {
+		biggerBits = zeroBits
+		smallerBits = oneBits
+	} else {
+		biggerBits = oneBits
+		smallerBits = zeroBits
+	}
+	return
+}
+
+func parseResults(var1 string, var2 string) int64 {
+	var1Int, _ := strconv.ParseInt(var1, 2, 64)
+	var2Int, _ := strconv.ParseInt(var2, 2, 64)
+
+	return var1Int * var2Int
 }
